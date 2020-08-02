06d7aadbebd954964aa20aef1cdd8e4eef29b658
delbury <592804814@qq.com>
2020-08-02 22:59:56 +0800

    更新audio处理class

--- src/libs/audio-processor.js
+++ src/libs/audio-processor.js
@@ -3,3 +3,26 @@
  * @author delbury
  * @version 1.0
  */
+
+ export default class AudioProcessor {
+  constructor(audio) {
+    this.audio = audio;
+    this.actx = new AudioContext();
+    this.nodes = []
+  }
+
+  // 初始化
+  init() {
+  }
+
+  // 创建源节点
+  createSourceNode(audio) {
+    this.nodes.push(this.actx.createMediaElementSource(audio));
+  }
+
+  // 创建分析节点
+
+  // 节点连接
+
+  // 关闭节点连接
+ }
