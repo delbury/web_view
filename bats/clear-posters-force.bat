baa0708923e8c8c94d8fd7924048f763bf1e11cb
delbury <592804814@qq.com>
2021-04-01 11:34:36 +0800

    优化清理poster脚本，新增只清除失效poster功能

--- bats/clear-posters-force.bat
+++ bats/clear-posters-force.bat
@@ -1,11 +1,11 @@
-@REM 清除失效的视频 poster
+@REM 强制清除所有的视频 poster
 @echo off
 echo Script starting...
 cd /d %~dp0
 call ./vars.bat
 
 cd /d %Scripts_Dir%
-node %Script_Clear_Posters% || goto error
+node %Script_Clear_Posters% -f || goto error
 exit
 
 :error
--- server/modules/clear-posters.js
+++ server/modules/clear-posters.js
@@ -1,5 +1,8 @@
 /**
  * @description 删除所有poster
+ * @argv
+ *  -f 强制全部清除
+ *  --log 打印 log
  */
 const { SOURCE_DIRS } = require('../config');
 const path = require('path').posix;
@@ -8,35 +11,66 @@ const fs = require('fs');
 const fsReaddir = promisify(fs.readdir);
 const fsStat = promisify(fs.stat);
 const fsUnlink = promisify(fs.unlink);
+const crypto = require('crypto');
 
-const fileReg = /\.(poster)/i;
+const posterFileReg = /\.poster$/i;
+const videoFileReg = /(\.mp4)$/i;
+const videoExt = '.mp4';
+const posterExt = '.poster';
 
 main();
 
 function main() {
+  const log = process.argv.includes('--log');
+  const force = process.argv.includes('-f');
   for(let dir of SOURCE_DIRS) {
     try {
       fs.accessSync(dir);
-      clearFile(dir);
+      clearFile(dir, force);
     } catch (err) {
-      console.log(err)
+      log && console.log(err);
     }
   }
 }
 
 // 读取文件夹
-async function clearFile(dir) {
+async function clearFile(dir, force = false) {
   const files = await fsReaddir(dir);
+
+  if(!force) {
+    files.sort((a, b) => {
+      const aExeced = videoFileReg.exec(a);
+      const bExeced = videoFileReg.exec(b);
+      const aExt = aExeced ? aExeced[0] : '';
+      const bExt = bExeced ? bExeced[0] : '';
+      
+      if(aExt === videoExt && bExt !== videoExt) return -1;
+      else if(aExt !== videoExt && bExt === videoExt) return 1;
+      else return 0;
+    }); // 排序，mp4 文件排在前面，先处理
+  }
+
+  const existSet = new Set(); // 记录存在的视频poster文件名
 
   for await(let item of files) {
     const fullPath = path.join(dir, item);
     const stat = await fsStat(fullPath);
 
-    // 判断是否是文件夹
     if(stat.isDirectory()) {
-      await clearFile(fullPath);
-    } else if(fileReg.test(item)) {
-      fsUnlink(fullPath);
+      // 判断是否是文件夹
+      await clearFile(fullPath, force);
+    } else if(posterFileReg.test(item)) {
+      // 是否是poster
+      console.log(fullPath)
+      if(force || !existSet.has(item)) {
+        fsUnlink(fullPath);
+      }
+    } else if(!force && videoFileReg.test(item)) {
+      // 是否是video文件
+      const md5 = crypto.createHash('md5');
+      md5.update(item);
+      const posterName = md5.digest('hex') + posterExt; // poster 名，不包括扩展名
+      existSet.add(posterName);
     }
   }
 }
