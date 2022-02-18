77532d6d017e07760914c1125007ef394e51843d
hujiqing <hujiqing@bytedance.com>
2022-02-18 17:50:03 +0800

    配置优化

--- server/config.js
+++ server/config.js
@@ -4,11 +4,7 @@ const HOST = ''; // 'http://192.168.0.103:4000'
 const INFO_FILES_DIR = path.join(__dirname, './modules');
 
 // 文件服务器
-const RESOURCE_DIR_NAME = 'pd';
-
-const RESOURCE_BASE_DIR = 'F:/资源';
-const SOURCE_DIR = path.join(RESOURCE_BASE_DIR, '/' + RESOURCE_DIR_NAME);
-const SOURCE_DIRS = [SOURCE_DIR, 'G:/BaiduNetdiskDownload']; // 全部静态文件夹
+const SOURCE_DIRS = ['F:/资源/pd', 'G:/BaiduNetdiskDownload']; // 全部静态文件夹
 
 const excludeErrorCodes = ['ECONNRESET', 'ECONNABORTED'];
 const ERROR_LOG_FILE = path.join(__dirname, './error.log');
@@ -16,9 +12,6 @@ const ERROR_LOG_FILE = path.join(__dirname, './error.log');
 module.exports = {
   HOST,
   INFO_FILES_DIR,
-  RESOURCE_BASE_DIR,
-  RESOURCE_DIR_NAME,
-  SOURCE_DIR,
   SOURCE_DIRS,
   excludeErrorCodes,
   ERROR_LOG_FILE,
